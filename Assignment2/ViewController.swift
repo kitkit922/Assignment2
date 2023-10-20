877e3e3adf499b1412fc5b0f7113f3c925d2838d
Tsz Kit Cheung <115593395+kitkit922@users.noreply.github.com>
2023-10-20 02:12:14 -0400

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Tsz Kit Cheung on 2023-10-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
